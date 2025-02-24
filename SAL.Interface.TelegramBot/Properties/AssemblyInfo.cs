1e3c42caa1e83a5a85d48e282f42f63b3a436676
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 03:56:07 +0300

    Unify plugins deployment

--- SAL.Interface.TelegramBot/Properties/AssemblyInfo.cs
+++ SAL.Interface.TelegramBot/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("e273a361-7201-4014-93cb-5ba9fc546118")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/SAL.Interface.TelegramBot")]
 #else
 
-[assembly: AssemblyTitle("Interface.TelegramBot")]
 [assembly: AssemblyDescription("Base interfaces for Telegram SAL plugins")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Interface.TelegramBot")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2017-2020")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2017-2025")]
 #endif
